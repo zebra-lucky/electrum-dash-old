16211247045e2399252facdcf97d5cb876a5c25c
zebra-lucky <zebra.lucky@gmail.com>
2020-04-04 22:06:59 +0300

    version to 3.3.8.6rc2

--- electrum_dash/version.py
+++ electrum_dash/version.py
@@ -1,8 +1,8 @@
 import re
 
 
-ELECTRUM_VERSION = '3.3.8.5' # version of the client package
-APK_VERSION = '3.3.8.5'      # read by buildozer.spec
+ELECTRUM_VERSION = '3.3.8.6rc2' # version of the client package
+APK_VERSION = '3.3.8.6'      # read by buildozer.spec
 
 PROTOCOL_VERSION = '1.4.2'   # protocol version requested
 
