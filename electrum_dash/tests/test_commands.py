47911881dc7a9b2165912634f82c9ec7fa5cb182
zebra-lucky <zebra.lucky@gmail.com>
2020-06-23 13:08:22 +0300

    test_commands.py: add test_createrawtransaction

--- electrum_dash/tests/test_commands.py
+++ electrum_dash/tests/test_commands.py
@@ -1,10 +1,16 @@
+import os
+import gzip
 import unittest
+import tempfile
+import shutil
 from unittest import mock
 from decimal import Decimal
 
 from electrum_dash.commands import Commands, eval_bool
 from electrum_dash import storage
-from electrum_dash.wallet import restore_wallet_from_text
+from electrum_dash.simple_config import SimpleConfig
+from electrum_dash.storage import WalletStorage
+from electrum_dash.wallet import restore_wallet_from_text, Wallet
 
 from . import TestCaseForTestnet
 
@@ -126,3 +132,51 @@ class TestCommandsTestnet(TestCaseForTestnet):
         for xkey1, xtype1 in xprvs:
             for xkey2, xtype2 in xprvs:
                 self.assertEqual(xkey2, cmds.convert_xkey(xkey1, xtype2))
+
+
+class TestTxCommandsTestnet(TestCaseForTestnet):
+
+    def setUp(self):
+        super(TestTxCommandsTestnet, self).setUp()
+        self.user_dir = tempfile.mkdtemp()
+        self.wallet_path = os.path.join(self.user_dir, 'wallet_ps1')
+        tests_path = os.path.dirname(os.path.abspath(__file__))
+        test_data_file = os.path.join(tests_path, 'data', 'wallet_ps1.gz')
+        shutil.copyfile(test_data_file, '%s.gz' % self.wallet_path)
+        with gzip.open('%s.gz' % self.wallet_path, 'rb') as rfh:
+            wallet_data = rfh.read()
+            wallet_data = wallet_data.decode('utf-8')
+        with open(self.wallet_path, 'w') as wfh:
+            wfh.write(wallet_data)
+        self.config = SimpleConfig({'electrum_path': self.user_dir})
+        self.config.set_key('dynamic_fees', False, True)
+        self.storage = WalletStorage(self.wallet_path)
+        self.wallet = Wallet(self.storage)
+
+    def tearDown(self):
+        super(TestTxCommandsTestnet, self).tearDown()
+        shutil.rmtree(self.user_dir)
+
+    def test_createrawtransaction(self):
+        HEX_TX_OUT1 = ('0200000001604079028e98106d5d88525fe42c950c2ef62d75cde8'
+                       '91e3d37ac81ed662ce0c0500000000ffffffff0300000000000000'
+                       '00116a0f0102030405060708090a0b0c0d0e0f40420f0000000000'
+                       '1976a9145f093709af6e9c15b080de359bdb7e78b1dd983088ac80'
+                       '841e00000000001976a91491282e4cd7d5e44867f93e8002393dac'
+                       '7c2a99db88ac00000000')
+        HEX_TX_OUT2 = ('0200000000030000000000000000116a0f0102030405060708090a'
+                       '0b0c0d0e0f40420f00000000001976a9145f093709af6e9c15b080'
+                       'de359bdb7e78b1dd983088ac80841e00000000001976a91491282e'
+                       '4cd7d5e44867f93e8002393dac7c2a99db88ac00000000')
+        HEX_TX_OUT3 = ('02000000000000000000')
+
+        inputs = [{'txid': '0cce62d61ec87ad3e391e8cd752df62e'
+                           '0c952ce45f52885d6d10988e02794060',
+                   'vout': 5}]
+        outputs = {'yUyx5hJsEwAukTdRy7UihU57rC37Y4y2ZX': 0.01,
+                   'yZYxxqJNR6fJ3fAT4Kyhye3A7G9kC19B9q': '0.02',
+                   'data': '0102030405060708090a0b0c0d0e0f'}
+        cmds = Commands(config=self.config, wallet=self.wallet, network=None)
+        assert cmds.createrawtransaction(inputs, outputs) == HEX_TX_OUT1
+        assert cmds.createrawtransaction([], outputs) == HEX_TX_OUT2
+        assert cmds.createrawtransaction([], {}) == HEX_TX_OUT3
